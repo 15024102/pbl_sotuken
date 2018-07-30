40cbaf033695886c0cd8d692c38fbf45d8953273
15024102 <15024102@mmm.muroran-it.ac.jp>
2018-07-30 15:09:03 +0900

    2回目

--- src/pbl/Pbl.java
+++ src/pbl/Pbl.java
@@ -15,7 +15,7 @@ public class Pbl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello");
     }
     
 }
