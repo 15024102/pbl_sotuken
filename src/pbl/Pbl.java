95c7a91a44165b5022b65a15d9c4a984d28b00ae
10000003-pbl-sotuken <PBLsotuken+10000003pbl@gmail.com>
2018-09-27 15:38:39 +0900

    24回目

--- src/pbl/Pbl.java
+++ src/pbl/Pbl.java
@@ -15,7 +15,7 @@ public class Pbl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("アカウント変更テスト5");
+        System.out.println("アカウント変更テスト6");
     }
     
 }
